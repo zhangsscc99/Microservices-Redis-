101d45a27f17546ce100a193fb61da9af8d651f7
Yuting Wang <yw4554@nyu.edu>
2024-04-11 00:03:10 -0500

    delayed tasks

--- heima-leadnews-basic/heima-file-starter/src/main/java/com/heima/file/config/MinIOConfigProperties.java
+++ heima-leadnews-basic/heima-file-starter/src/main/java/com/heima/file/config/MinIOConfigProperties.java
@@ -8,6 +8,15 @@ import java.io.Serializable;
 
 @Data
 @ConfigurationProperties(prefix = "minio")  // 文件上传 配置前缀file.oss
+//适配任何需求的延迟任务
+//redis
+//并发 数据库锁机制
+//分布式锁。
+//乐观锁。
+//悲观锁。
+//redis实现分布式锁。
+//提升执行效率。
+//管道技术。多个redis合并为一个。
 public class MinIOConfigProperties implements Serializable {
 
     private String accessKey;
